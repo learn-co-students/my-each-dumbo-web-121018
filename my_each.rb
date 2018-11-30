ceebb76caf04ea4cab2a5d328f4efa1c567b00a2
jonathan choi <jisung594@gmail.com>
2018-11-30 04:54:39 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,15 @@
-def my_each # put argument(s) here
-  # code here
+arr = []
+
+def my_each(arr)
+  if block_given?
+    i = 0
+    while i < arr.length
+      yield arr[i]
+      i += 1
+    end
+    
+    return arr
+  end
 end
+
+my_each(arr) { |ele| ele }

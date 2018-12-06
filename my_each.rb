f233938483b46313a238df8d79a5012f8df42bed
Jun Haeng Lee <johnhaeng@gmail.com>
2018-12-06 17:10:56 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,14 @@
-def my_each # put argument(s) here
-  # code here
+def hello(array)
+  i = 0
+  collection = []
+  while i < array.length
+    collection << yield(array[i])
+    i += 1
+  end
+  
+  collection
 end
+
+
+
+
